39545b52739c4d66ba195a6c361c9bd315aaf995
TaghreedSamir94 <taghreedsamir49@gmail.com>
2024-08-15 23:16:56 +0300

    Task 1 MVC

---
+++ Task1/MVCTASk/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTASk.Models;
+
+namespace MVCTASk.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ask model
+            StudentBL studentBL = new StudentBL();
+            List<Student> studentList = studentBL.GetAll();
+            //send view
+            return View("AllStudentsList", studentList);
+        }
+
+
+        public IActionResult Details(int Id)
+        {
+            //ask model 
+            StudentBL studentBL = new StudentBL();
+            Student? std1 = studentBL.GetById(Id);
+            //send view
+            return View("details", std1);
+        }
+    }
+}
